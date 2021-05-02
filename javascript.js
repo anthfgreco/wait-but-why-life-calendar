d645452ecaa6afa0953bee461d227756f2adf0c0
popTheSmart <popthesmart@hotmail.com>
2021-05-02 09:44:22 -0400

    circles!

--- javascript.js
+++ javascript.js
@@ -13,22 +13,36 @@ String.prototype.format = function () {
   });
 };
 
+function calculateCircleDiameter(numCircles) {
+  circle_div = document.getElementById("circle-div");
+  var divWidth = circle_div.offsetWidth;
+  var divHeight = circle_div.offsetHeight;
+  return Math.floor(Math.sqrt(divWidth * 0.8 * divHeight / numCircles))
+}
+
 function addCircle() {
   circle = document.createElement("div");
   circle.className = "circle";
-  circle.style.border = "3px solid #FF0000";
-  circlediv = document.getElementById("circle-div");
-  document.getElementById("circle-div").appendChild(circle, circlediv);
+  var diameter = calculateCircleDiameter(80);
+  circle.style.width = "{}px".format(diameter);
+  circle.style.height = "{}px".format(diameter);
+  document.getElementById("circle-div").appendChild(circle);
 }
 
-function updateCircles() {
-  for (let i = 0; i < 80; i++) {
+function updateCircles(age) {
+  for (let i = 0; i < age; i++) {
     addCircle();
   }
-  console.log("Screen Width: " + screen.width);
-  console.log("Screen Height: " + screen.height);
-  console.log("Available Screen Width: " + screen.availWidth);
-  console.log("Available Screen Height: " + screen.availHeight);
 }
 
-updateCircles();
+function onResize() {
+  circle_div = document.getElementById("circle-div");
+  circle_div.remove();
+
+  new_circle_div = document.createElement("div");
+  new_circle_div.id = "circle-div";
+  document.getElementById("body").appendChild(new_circle_div);
+  updateCircles(80);
+}
+
+updateCircles(80);
