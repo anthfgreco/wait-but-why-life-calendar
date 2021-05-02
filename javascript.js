3454f9e2458f248c1ca8f9c916c5feb8a54dd3a0
popTheSmart <popthesmart@hotmail.com>
2021-05-02 15:53:35 -0400

    add basic styling

--- javascript.js
+++ javascript.js
@@ -13,34 +13,41 @@ String.prototype.format = function () {
   });
 };
 
+const map = (value, x1, y1, x2, y2) => (value - x1) * (y2 - x2) / (y1 - x1) + x2;
+
 /**************************************************************************************************************
 ***  Main Program
 ***************************************************************************************************************/
 
-var numCircles = 980;
+var numCircles = 4160/52;    // Number of circles to be displayed on canvas
+var margin = 0;           // Controls space between circles
 
 function getDiameter() {
   circle_div = document.getElementById("circle-div");
   var divWidth = circle_div.offsetWidth;
   var divHeight = circle_div.offsetHeight;
-  return Math.floor(Math.sqrt((divWidth * 0.96 * divHeight) / numCircles));
+  console.log(divWidth, divHeight);
+  return Math.floor(Math.sqrt((divWidth * divHeight) / numCircles)) / 1.1;
 }
 
 function setup() {
   var canvas = createCanvas(windowWidth, windowHeight*0.9);
   canvas.id("circle-div");
-  background(0, 0, 0);
+  background(240, 248, 255);
 
-  var diameter = getDiameter();
-  var x = diameter/2;
-  var y = diameter/2;
+  var diameter = getDiameter() - margin;
+  console.log(diameter)
+  var x = diameter/2 + margin;
+  var y = diameter/2 + margin;
   
   for (let i = 0; i < numCircles; i++) {
+    let c = color(0, 0, map(i, 0, numCircles, 0, 255));
+    fill(c);
     circle(x, y, diameter);
-    x += diameter;
+    x += diameter + margin;
     if (x > windowWidth-diameter/2) {
-      x = diameter/2;
-      y += diameter;
+      x = diameter/2 + margin; //default
+      y += diameter + margin;
     }
   }
 }
