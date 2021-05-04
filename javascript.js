417a559ac9b55afc23189abd38ea72d2508f5749
popTheSmart <popthesmart@hotmail.com>
2021-05-04 15:31:31 -0400

    add basic inputs

--- javascript.js
+++ javascript.js
@@ -20,23 +20,25 @@ const map = (value, x1, y1, x2, y2) => (value - x1) * (y2 - x2) / (y1 - x1) + x2
 ***************************************************************************************************************/
 
 var numCircles = 4160/52;    // Number of circles to be displayed on canvas
-var margin = 0;           // Controls space between circles
+var margin = 4;           // Controls space between circles
 
 function getDiameter() {
   circle_div = document.getElementById("circle-div");
   var divWidth = circle_div.offsetWidth;
   var divHeight = circle_div.offsetHeight;
-  console.log(divWidth, divHeight);
   return Math.floor(Math.sqrt((divWidth * divHeight) / numCircles)) / 1.1;
 }
 
 function setup() {
-  var canvas = createCanvas(windowWidth, windowHeight*0.9);
+  // Set up canvas
+  var canvas = createCanvas(windowWidth*0.9, windowHeight*0.9);
   canvas.id("circle-div");
-  background(240, 248, 255);
+  var x = (windowWidth - width) / 2;
+  var y = 20 + (windowHeight - height) / 2;
+  canvas.position(x, y);
+  background(240, 248, 0);
 
   var diameter = getDiameter() - margin;
-  console.log(diameter)
   var x = diameter/2 + margin;
   var y = diameter/2 + margin;
   
@@ -45,7 +47,7 @@ function setup() {
     fill(c);
     circle(x, y, diameter);
     x += diameter + margin;
-    if (x > windowWidth-diameter/2) {
+    if (x > windowWidth*0.9 - diameter/2) {
       x = diameter/2 + margin; //default
       y += diameter + margin;
     }
@@ -55,3 +57,22 @@ function setup() {
 function windowResized() {
   setup();
 }
+
+
+
+// Initializes date picker to current date
+document.querySelector("#bdayPicker").valueAsDate = new Date();
+
+// Bday picker listener
+document.getElementById("bdayPicker").addEventListener("change", function() {
+  var input = this.value;
+  var dateEntered = new Date(input);
+  console.log(input); //e.g. 2015-11-13
+  console.log(dateEntered); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
+});
+
+// Expected age listener
+document.getElementById("expectedAge").addEventListener("change", function() {
+  var input = this.value;
+  console.log(input);
+});
